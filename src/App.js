ce6761c033db4e081aca443514eef463cb83208e
kekkei9 <kekkei2002@gmail.com>
2024-06-19 08:39:03 +0700

    fix build

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './styles/button.style.scss'
-import './styles/table.style.scss'
-import './styles/img.style.scss'
+import "./styles/button.style.scss";
+import "./styles/img.style.scss";
+import "./styles/table.style.scss";
 
-import React, { Suspense } from 'react'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import './App.scss'
-import store from './redux/store'
-import AppRouter from './routers/AppRouter'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-functionafdafd App() {
+import React, { Suspense } from "react";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import store from "./redux/store";
+import AppRouter from "./routers/AppRouter";
+
+function App() {
   return (
     <HashRouter>
-    RYHGOHOSAHOAHOGIHSOGHSOIHGOISEGHIEUHGIHGUIEHGHEWAHGEWAIUH
       <Provider store={store}>
         <Suspense fallback="loading">
           <Header />
-            <AppRouter />
-          <Footer/>
+          <AppRouter />
+          <Footer />
         </Suspense>
       </Provider>
     </HashRouter>
-  )
+  );
 }
 
-export default App
+export default App;
