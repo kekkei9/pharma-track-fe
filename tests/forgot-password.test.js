0b5b5ab201ddd0e882b3d9b396d49e4fa54c1c11
kekkei9 <kekkei2002@gmail.com>
2024-06-19 19:13:32 +0700

    fixed test case

---
+++ tests/forgot-password.test.js
@@ -0,0 +1,58 @@
+// __tests__/login.test.js
+
+const launch = require("puppeteer").launch;
+
+describe("Login", () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await launch({
+      headless: true,
+      slowMo: 100,
+      devtools: true,
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  let forgotPasswordNavigateSelector =
+    "#root > div.LoginPage.tw-flex.tw-flex-col.tw-items-center > div > div.login-action-container.tw-flex.tw-flex-col.tw-items-center > div.LoginFormContainer > div > form > div.tw-text-red-500.tw-mt-3.tw-ml-3.tw-self-start.forgot-password";
+
+  let emailInputSelector =
+    "#root > div.LoginPage.tw-flex.tw-flex-col.tw-items-center > div > form > div.field-container > div > div > div.ant-col.ant-form-item-control.css-3rel02 > div.ant-form-item-control-input > div > input";
+
+  it("should display forgot password button", async () => {
+    await page.goto("https://kekkei9.github.io/pharma-track-fe/#/login");
+    await page.waitForSelector(forgotPasswordNavigateSelector);
+    const pageTitle = await page.$eval(
+      forgotPasswordNavigateSelector,
+      (e) => e.innerHTML
+    );
+    expect(pageTitle).toMatch("Quên mật khẩu");
+  }, 10000);
+
+  it("should allow the user to log in", async () => {
+    await page.goto("https://kekkei9.github.io/pharma-track-fe/#/login");
+    await page.waitForSelector(forgotPasswordNavigateSelector);
+    await page.click(forgotPasswordNavigateSelector);
+
+    await page.type(emailInputSelector, "kekkei16102002@gmail.com");
+
+    await page.click(
+      "#root > div.LoginPage.tw-flex.tw-flex-col.tw-items-center > div > form > div.submit-container > div > div > div > div > div > button"
+    );
+
+    //back to login page
+
+    await page.waitForSelector(forgotPasswordNavigateSelector);
+    const pageTitle = await page.$eval(
+      forgotPasswordNavigateSelector,
+      (e) => e.innerHTML
+    );
+    expect(pageTitle).toMatch("Quên mật khẩu");
+  }, 40000);
+});
